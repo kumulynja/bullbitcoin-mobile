3e9f880fb647c4296cbb7d163a1ba48fb7dd4211
saiy2k <saiy2k@gmail.com>
2024-08-29 13:34:40 +0530

    Migration: Patch migration structural change (untested code)

--- lib/_pkg/migrations/migration.dart
+++ lib/_pkg/migrations/migration.dart
@@ -1,4 +1,5 @@
 import 'package:bb_mobile/_pkg/migrations/migration0_1to0_2.dart';
+import 'package:bb_mobile/_pkg/migrations/migration0_2_2to0_2_3.dart';
 import 'package:bb_mobile/_pkg/migrations/migration0_2to0_3.dart';
 import 'package:bb_mobile/_pkg/storage/hive.dart';
 import 'package:bb_mobile/_pkg/storage/secure_storage.dart';
@@ -11,18 +12,29 @@ Future<void> doMigration(
 ) async {
   print('fromVersion $fromVersion; toVersion $toVersion');
 
-  // await doMigration0_1to0_2(
-  //   secureStorage,
-  //   hiveStorage,
-  // );
+  final fromPatch = int.tryParse(fromVersion.split('.').last)!;
+  final toPatch = int.tryParse(toVersion.split('.').last)!;
 
-  if (toVersion.startsWith('0.2') && fromVersion.startsWith('0.1')) {
-    await doMigration0_1to0_2(secureStorage, hiveStorage);
+  if (toVersion.startsWith('0.2')) {
+    if (fromVersion.startsWith('0.1')) {
+      await doMigration0_1to0_2(secureStorage, hiveStorage);
+      if (toPatch >= 3) {
+        await doMigrationPatch0_2_2to0_2_3(secureStorage, hiveStorage);
+      }
+    } else if (fromVersion.startsWith('0.2')) {
+      if (fromPatch <= 2 && toPatch >= 3) {
+        await doMigrationPatch0_2_2to0_2_3(secureStorage, hiveStorage);
+      }
+    }
   } else if (toVersion.startsWith('0.3')) {
     if (fromVersion.startsWith('0.1')) {
       await doMigration0_1to0_2(secureStorage, hiveStorage);
+      await doMigrationPatch0_2_2to0_2_3(secureStorage, hiveStorage);
       await doMigration0_2to0_3(secureStorage, hiveStorage);
     } else if (fromVersion.startsWith('0.2')) {
+      if (fromPatch <= 2) {
+        await doMigrationPatch0_2_2to0_2_3(secureStorage, hiveStorage);
+      }
       await doMigration0_2to0_3(secureStorage, hiveStorage);
     }
   } else if (toVersion.startsWith('0.4')) {
---
+++ lib/_pkg/migrations/migration0_2_2to0_2_3.dart
@@ -0,0 +1,107 @@
+// Change 1: move ln swap fields from SwapTx to SwapTx.lnSwapDetails
+import 'dart:convert';
+
+import 'package:bb_mobile/_model/swap.dart';
+import 'package:bb_mobile/_model/wallet.dart';
+import 'package:bb_mobile/_pkg/storage/hive.dart';
+import 'package:bb_mobile/_pkg/storage/secure_storage.dart';
+import 'package:bb_mobile/_pkg/storage/storage.dart';
+import 'package:bb_mobile/_pkg/wallet/repository/sensitive_storage.dart';
+import 'package:boltz_dart/boltz_dart.dart';
+
+Future<void> doMigrationPatch0_2_2to0_2_3(
+  SecureStorage secureStorage,
+  HiveStorage hiveStorage,
+) async {
+  print('Migration: 0.2.2 to 0.2.3');
+
+  final (walletIds, walletIdsErr) =
+      await hiveStorage.getValue(StorageKeys.wallets);
+  if (walletIdsErr != null) throw walletIdsErr;
+
+  final walletIdsJson = jsonDecode(walletIds!)['wallets'] as List<dynamic>;
+  if (walletIdsJson.isEmpty) throw 'No Wallets found';
+
+  final WalletSensitiveStorageRepository walletSensitiveStorageRepository =
+      WalletSensitiveStorageRepository(secureStorage: secureStorage);
+
+  final List<Wallet> wallets = [];
+
+  for (final walletId in walletIdsJson) {
+    // print('walletId: $walletId');
+    final (jsn, err) = await hiveStorage.getValue(walletId as String);
+    if (err != null) throw err;
+
+    final Map<String, dynamic> walletObj =
+        jsonDecode(jsn!) as Map<String, dynamic>;
+
+    final updatedWalletObj =
+        await updateSwaps(walletObj, walletSensitiveStorageRepository);
+
+    final w = Wallet.fromJson(updatedWalletObj);
+    wallets.add(w);
+  }
+
+  final walletObjs = wallets.map((w) => w.toJson()).toList();
+  final List<String> ids = [];
+  for (final w in walletObjs) {
+    final id = w['id'] as String;
+    ids.add(id);
+    final _ = await hiveStorage.saveValue(
+      key: id,
+      value: jsonEncode(w),
+    );
+  }
+
+  final idsJsn = jsonEncode({
+    'wallets': [...ids],
+  });
+  final _ = await hiveStorage.saveValue(
+    key: StorageKeys.wallets,
+    value: idsJsn,
+  );
+
+  // Finally update version number to next version
+  await secureStorage.saveValue(key: StorageKeys.version, value: '0.3');
+}
+
+Future<Map<String, dynamic>> updateSwaps(
+  Map<String, dynamic> walletObj,
+  WalletSensitiveStorageRepository walletSensitiveStorageRepository,
+) async {
+  walletObj['transactions'] = walletObj['transactions']
+      .map((tx) => tx as Map<String, dynamic>)
+      .map((tx) {
+    if (tx['swapTx'] != null && tx['swapTx']['invoice'] != null) {
+      final isSubmarine = tx['swapTx']['isSubmarine'] == true;
+      if (isSubmarine)
+        tx['swapTx']['lockupTxid'] = tx['swapTx']['txid'];
+      else
+        tx['swapTx']['claimTxid'] = tx['swapTx']['txid'];
+
+      tx['swapTx']['lnSwapDetails'] = LnSwapDetails(
+        swapType: tx['swapTx']['isSubmarine'] == true
+            ? SwapType.submarine
+            : SwapType.reverse,
+        invoice: tx['swapTx']['invoice'] as String,
+        boltzPubKey: tx['swapTx']['boltzPubkey'] as String,
+        keyIndex: tx['swapTx']['keyIndex'] != null
+            ? tx['swapTx']['keyIndex'] as int
+            : 0,
+        myPublicKey: tx['swapTx']['publicKey'] as String,
+        electrumUrl: tx['swapTx']['electrumUrl'] as String,
+        locktime: tx['swapTx']['locktime'] as int,
+        sha256: tx['swapTx']['sha256'] != null
+            ? tx['swapTx']['sha256'] as String
+            : '', // TODO: Should do this?
+        // hash160: tx['swapTx']['hash160'] as String, // TODO: Should do this?
+        // blindingKey:
+        //     tx['swapTx']['blindingKey'] as String, // TODO: Should do this?
+      ).toJson();
+      return tx;
+    }
+    return tx;
+  }).toList();
+
+  return walletObj;
+}
